5db4b7d12f6c18968bb46b43cb7f75e1ddd25c14
CV2 CV2 <2cv36985@gmail.com>
2024-06-08 16:41:30 -0500

    Agrego estructura básia sidebar y dependencias

---
+++ src/utils/dataEstatica.jsx
@@ -0,0 +1,138 @@
+import { v } from "../styles/variables";
+import {
+  AiOutlineHome,
+  AiOutlineSetting,
+} from "react-icons/ai";
+
+export const DesplegableUser = [
+  {
+    text: "Mi perfil",
+    icono: <v.iconoUser/>,
+    tipo: "miperfil",
+  },
+  {
+    text: "Configuracion",
+    icono: <v.iconoSettings/>,
+    tipo: "configuracion",
+  },
+  {
+    text: "Cerrar sesión",
+    icono: <v.iconoCerrarSesion/>,
+    tipo: "cerrarsesion",
+  },
+];
+
+
+
+//data SIDEBAR
+export const LinksArray = [
+  {
+    label: "Estudiantes",
+    icon: <v.iconoperfil />,
+    to: "/",
+  },
+  {
+    label: "Registros",
+    icon: <v.iconocheck />,
+    to: "/kardex",
+  },
+  {
+    label: "Informes",
+    icon: <v.iconoinforme />,
+    to: "/informes",
+  },
+  {
+    label: "Reportes",
+    icon: <v.iconoreportes />,
+    to: "/reportes",
+  },
+ 
+];
+export const SecondarylinksArray = [
+  {
+    label: "Configuración",
+    icon: <AiOutlineSetting />,
+    to: "/configurar",
+  },
+
+];
+//temas
+export const TemasData = [
+  {
+    icono: "🌞",
+    descripcion: "light",
+   
+  },
+  {
+    icono: "🌚",
+    descripcion: "dark",
+    
+  },
+];
+
+//data configuracion
+export const DataModulosConfiguracion =[
+  {
+    title:"Estudiantes",
+    subtitle:"Gestionar a los estudiantes",
+    icono:"https://i.ibb.co/85zJ6yG/caja-del-paquete.png",
+    link:"/configurar/productos",
+   
+  },
+  {
+    title:"Registro",
+    subtitle:"Puede ver los registros de todos los estudiantes",
+    icono:"https://i.ibb.co/5vgZ0fX/hombre.png",
+    link:"/configurar/usuarios",
+   
+  },
+
+  {
+    title:"Tu empresa",
+    subtitle:"configura tus opciones básicas",
+    icono:"https://i.ibb.co/x7mHPgm/administracion-de-empresas.png",
+    link:"/configurar/empresa",
+    
+  },
+  {
+    title:"Categoria de productos",
+    subtitle:"asigna categorias a tus productos",
+    icono:"https://i.ibb.co/VYbMRLZ/categoria.png",
+    link:"/configurar/categorias",
+    
+  },
+  {
+    title:"Marca de productos",
+    subtitle:"gestiona tus marcas",
+    icono:"https://i.ibb.co/1qsbCRb/piensa-fuera-de-la-caja.png",
+    link:"/configurar/marca",
+   
+  },
+
+]
+//tipo usuario
+export const TipouserData = [
+  {
+    descripcion: "empleado",
+    icono: "🪖",
+  },
+  {
+    descripcion: "administrador",
+    icono: "👑",
+  },
+];
+//tipodoc
+export const TipoDocData = [
+  {
+    descripcion: "Ci",
+    icono: "🪖",
+  },
+  {
+    descripcion: "Libreta electoral",
+    icono: "👑",
+  },
+  {
+    descripcion: "Otros",
+    icono: "👑",
+  },
+];
