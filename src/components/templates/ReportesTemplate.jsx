52769e67a95ffd51c7f73b24cc4b13fa4a097662
CV2 CV2 <2cv36985@gmail.com>
2024-06-25 03:35:17 -0500

    Agrego un nuevo elemento en Reportes

--- src/components/templates/ReportesTemplate.jsx
+++ src/components/templates/ReportesTemplate.jsx
@@ -1,14 +1,38 @@
 import styled from "styled-components";
-import Estudiantes from "../../supabase/Estudiantes"
+import Estudiantes from "../../supabase/Estudiantes";
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
 export function ReportesTemplate() {
   return (
     <Container>
-      <div>
-      <Estudiantes />
-      </div>
-      
-    </Container>
-  );
+    <div
+    className="modal show"
+    style={{ display: 'block', position: 'initial' }}
+  >
+    <Modal.Dialog>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal title</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <p>Modal body text goes here.</p>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary">Close</Button>
+        <Button variant="primary">Save changes</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  </div>
+  
+  </Container>
+
+  
+
+)
 }
 const Container = styled.div`
   display: flex;
